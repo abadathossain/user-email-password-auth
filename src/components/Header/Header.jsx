ddb6763c068222916e39bf33d1d0527d927253cd
Abadat Hossain <abadat26@gmail.com>
2024-10-14 11:11:01 +0600

    register form almost done and data get from field

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
 
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 justify-center">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -36,7 +36,7 @@ export default function Header() {
 
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Email Auth</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -45,9 +45,7 @@ export default function Header() {
 
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
+          
         </div>
     )
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export default function Register() {
+    const handleSubmit=e=>{
+        e.preventDefault()
+        const name=e.target.name.value
+        const email=e.target.email.value
+        const password=e.target.password.value
+        console.log(name, email, password)
+    }
   return (
     <div>
         <h5 className='text-4xl font-bold text-center'>Please Register</h5>
+        <div className="">
+            <div className="mx-auto md:w-1/2">
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form onSubmit={handleSubmit}>
+                    <input className="mb-4 w-full  py-2 px-4" type="text" name="name" placeholder="Your Name" id="" required />
+                    <br />
+                    <input className="mb-4 w-full  py-2 px-4" type="email" name="email" placeholder="Email Address" id="" required />
+                    <br />
+                    <div className="mb-4 relative border">
+                        <input
+                            className="w-full py-2 px-4"
+                            
+                            name="password"
+                            placeholder="Password"
+                            id="" required />
+                        <span className="absolute top-3 right-2">
+                            
+
+                        </span>
+                    </div>
+                    <br />
+                    <div className="mb-2">
+                        <input type="checkbox" name="terms" id="terms" />
+                        <label className="ml-2" htmlFor="terms">Accept our <a href="">Terms and Conditions</a></label>
+                    </div>
+                    <br />
+                    <input className="btn btn-secondary mb-4 w-full" type="submit" value="Register" />
+                </form>
+                
+                <p>Already have an account? Please <Link to="/login">Login</Link></p>
+            </div>
+        </div>
     </div>
   )
 }
